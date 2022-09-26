20d33b765a9fb2cc266248cafc7dd0547c60b34a
jims-git <jiminma50@hotmail.com>
2022-09-25 23:26:30 -0400

    getline

--- myprogram.cpp
+++ myprogram.cpp
@@ -11,13 +11,20 @@
 using std::cin;
 using std::cout;
 using std::endl;
+using std::string;
 
 int main() {
 	int a=5;
 	int b=10;
 	int c=sumOfTwoNumbers(a, b);
-  cout << "Hello World!";
-  cout << "\n\nc = " << c << endl;
-  cout << endl;
-  return 0;
+	cout << "Hello World!";
+	cout << "\n\nc = " << c << endl;
+	cout << endl;
+	
+	string greet;
+	cout << "What is your name ? ";
+	getline(cin, greet);
+	cout << "Thanks " << greet << endl;
+	
+	return 0;
 }
