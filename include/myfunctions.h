47c3401297bdbb2db2988803451dc19183613a54
jims-git <jiminma50@hotmail.com>
2022-10-02 16:58:26 -0400

    Added header guard

--- include/myfunctions.h
+++ include/myfunctions.h
@@ -1,3 +1,10 @@
+// Header Guard
+#ifndef MYFUNCTIONS_H
+#define MYFUNCTIONS_H
+
 // Function that returns the sum of two numbers
 
 int sumOfTwoNumbers(int a, int b);
+
+#endif /* !MYFUNCTIONS_H */
+
